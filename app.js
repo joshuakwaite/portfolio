cc6d174b22364f379318a7f12dccf34fce9ac418
Josh Waite <kalewaite@gmail.com>
2017-02-22 09:11:46 -0700

    changed home page

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.config(["$routeProvider", function ($routeProvider) {
             controller: "portfolioController"
         })
         .otherwise({
-            redirectTo: "/home"
+            redirectTo: "/about"
         })
 }])
